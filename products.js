fa3ba2ce5c32af1f8d8a609117dc7c48d2cc5aa9
Staz1x <fogel.stazix@gmail.com>
2023-03-23 13:36:33 +0100

    small changes

--- products.js
+++ products.js
@@ -20,19 +20,19 @@ function showProducts(data) {
         const element = document.createElement("div");
         element.innerHTML = 
             '<div class="product-button" data-bs-toggle="modal"' + ' data-bs-target="#product-modal' + i + '">' +
-                '<p>' + data[i].title + '</p>' +
-                '<img src=' + data[i].image + ' class="img-fluid" alt="BILD SAKNAS">' +
-                '<p class="productPrice">' + data[i].price + '</p>'+
+                '<p class="product-title">' + data[i].title + '</p>' +
+                '<img src=' + data[i].image + ' class="product-image" alt="BILD SAKNAS">' +
+                '<p class="product-price">' + data[i].price + '</p>'+
             '</div>' +
             '<div class="modal fade modal-xl" id="product-modal' + i + '">' +
                 '<div class="modal-dialog modal-dialog-centered">' +
                     '<div class="modal-content">' +
                         '<div class="modal-body">' +
                             '<div class="product-modal">' +
-                                '<h1 class="productPrice">' + data[i].price + " €" + '</h1>' + '<br>' +
-                                '<p class="productCategory">' + data[i].category + '</p>' + '<br>' +
-                                '<h2 class="productTitle">' + data[i].title + '</h2>' + '<br>' +
-                                '<img src=' + data[i].image + ' class="img-fluid" alt="BILD SAKNAS">' + '<br>' +
+                                '<h1 class="product-price">' + data[i].price + " €" + '</h1>' + '<br>' +
+                                '<p class="product-category">' + data[i].category + '</p>' + '<br>' +
+                                '<h2 class="product-title">' + data[i].title + '</h2>' + '<br>' +
+                                '<img src=' + data[i].image + ' class="product-image" alt="BILD SAKNAS">' + '<br>' +
                                 '<p class="description">' + data[i].description + '</p>' +
                                 '<div class="modal-footer justify-content-center"> ' +
                                     '<button class="btn btn-primary opacity-90 col-2 mx-3" id="order-button">Order</button> ' +
