d67e8a510e1ff9a00c5dae3cb115dec891de6a60
Ida Grufman Johansson <johansson.c.ida@gmail.com>
2023-03-24 15:09:37 +0100

    Update

--- script/script.js
+++ script/script.js
@@ -71,7 +71,7 @@ orderForm.addEventListener("submit", (e) => {
   if (firstName.value == "" || lastname.value == "") {
     alert("Ensure you input a value in both fields!");
   } else {
-    // perform operation with form input
+    
     alert("This form has been successfully submitted!");
     console.log(
       `This form has a first name of ${firstName.value} and lastname of ${lastname.value}`
