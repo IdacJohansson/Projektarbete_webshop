53862c69dadfda5bba75eafa212a63678a1be1db
Ida Grufman Johansson <johansson.c.ida@gmail.com>
2023-04-03 15:35:29 +0200

    Update form

--- script/script.js
+++ script/script.js
@@ -1,72 +1,3 @@
-// LocalStorage
-    /*const orderForm = document.getElementById('orderForm'); 
-    orderForm.addEventListener('submit', function(e){
-        e.preventDefault(); 
-
-        
-        const firstname = document.getElementById("firstname").value;
-        const lastname = document.getElementById("lastname").value;
-        const address = document.getElementById("address").value;
-        const city = document.getElementById("city").value;
-        const zip = document.getElementById("zip").value;
-        const phone = document.getElementById("phone").value;
-        const email = document.getElementById("email").value; 
-        
-
-        const zipPattern = /^\d{3}\s\d{2}$/;
-        const emailPattern = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
-        const phonePattern = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
-
-        if (firstname.length < 2 || firstname.length > 50) {
-            document.getElementById('fn').innerText = "First name must be 2-50 characters\r\n";
-        } else {
-            localStorage.setItem("firstname", firstname); 
-            document.getElementById('fn').innerText = "";
-        }
-        if (lastname.length < 2 || lastname.length > 50) {
-            document.getElementById('ln').innerText = "Last name must be 2-50 characters.\r\n";
-        } else {
-            localStorage.setItem("lastname", lastname);
-            document.getElementById('ln').innerText = "";
-        }
-        if (address.length < 4 || address.length > 50) {
-            document.getElementById('ad').innerText = "Address must be 4-50 characters.\r\n";
-            
-        } else {
-            localStorage.setItem("address", address); 
-            document.getElementById('ad').innerText = "";
-        }
-        if (zip == "" || !zipPattern.test(zip)){
-            document.getElementById('zi').innerText = "Please enter a valid zip code (format: xxx xx).";
-            
-        } else{
-            localStorage.setItem("zip", zip); 
-            document.getElementById('zi').innerText = ""; 
-        }
-        if (city.length < 2 || city.length > 50 ){
-            document.getElementById('ci').innerText = "City must be 2-50 characters.\r\n";
-            
-        } else{
-            localStorage.setItem("city", city); 
-            document.getElementById('ci').innerText = ""; 
-        }
-        if (phone == "" || !phonePattern.test(phone) || phone.length > 50 ){
-            document.getElementById('ph').innerText = "Please enter valid phone number.\r\n";
-            
-        } else{
-            localStorage.setItem("phone", phone); 
-            document.getElementById('ph').innerText = ""; 
-        }
-        if (email == "" || !emailPattern.test(email) ){
-            document.getElementById('em').innerText = "Please enter a valid e-mail address.\r\n";
-            
-        } else{
-            localStorage.setItem("email", email); 
-            document.getElementById('em').innerText = ""; 
-        }
-        
-        
-    }); */
 
     function formValidationCheck(){
 
@@ -87,52 +18,54 @@
             const zipPattern = /^\d{3}\s\d{2}$/;
             const emailPattern = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
             const phonePattern = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+
+            const isValid = true; 
     
             if (firstname.length < 2 || firstname.length > 50) {
                 document.getElementById('fn').innerText = "First name must be 2-50 characters\r\n";
-                return false; 
+                return !isValid; 
             } else{
                 localStorage.setItem("firstname", firstname);
                 document.getElementById('fn').innerText = "";
             }
             if (lastname.length < 2 || lastname.length > 50) {
                 document.getElementById('ln').innerText = "Last name must be 2-50 characters.\r\n"; 
-                return false;
+                return !isValid;
             } else {
                 localStorage.setItem("lastname", lastname);
                 document.getElementById('ln').innerText = "";
             }
             if (address.length < 4 || address.length > 50) {
                 document.getElementById('ad').innerText = "Address must be 4-50 characters.\r\n";
-                return false;
+                return !isValid;
             } else {
                 localStorage.setItem("address", address); 
                 document.getElementById('ad').innerText = "";
             }
             if (zip == "" || !zipPattern.test(zip)){
                 document.getElementById('zi').innerText = "Please enter a valid zip code (format: xxx xx)."; 
-                return false;
+                return !isValid;
             } else{
                 localStorage.setItem("zip", zip); 
                 document.getElementById('zi').innerText = ""; 
             }
             if (city.length < 2 || city.length > 50 ){
                 document.getElementById('ci').innerText = "City must be 2-50 characters.\r\n"; 
-                return false;
+                return !isValid;
             } else{
                 localStorage.setItem("city", city); 
                 document.getElementById('ci').innerText = ""; 
             }
             if (phone == "" || !phonePattern.test(phone) || phone.length > 50 ){
                 document.getElementById('ph').innerText = "Please enter valid phone number.\r\n";
-                return false;  
+                return !isValid;  
             } else{
                 localStorage.setItem("phone", phone); 
                 document.getElementById('ph').innerText = ""; 
             }
             if (email == "" || !emailPattern.test(email) ){
                 document.getElementById('em').innerText = "Please enter a valid e-mail address.\r\n";
-                return false;
+                return !isValid;
                 
             } else{
                 localStorage.setItem("email", email);  
@@ -140,15 +73,13 @@
             }
 
             window.location.href = "order-confirmation.html"; 
-            return true; 
+            return isValid; 
             
             
             
             
             
         }); 
-
-
 
     }
 
@@ -173,10 +104,6 @@
     const e = localStorage.getItem("email"); 
     document.getElementById("email").textContent = e;
 
-
-    
-   
-
     for (let i = 0; i < localStorage.length; i++) {
         console.log("Key: " + localStorage.key(i));
         console.log("Value: "+ localStorage.getItem(localStorage.key(i)));
@@ -186,25 +113,6 @@
 
 
 
-// Gallery carusell
-/*let slideIndex = 0;
-        function showSlides() {
-        let i;
-        let slides = document.getElementsByClassName("mySlides");
-        let dots = document.getElementsByClassName("dot");
-        for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-        }
-        slideIndex++;
-        if (slideIndex > slides.length) {slideIndex = 1}    
-        for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-        }
-        slides[slideIndex-1].style.display = "block";  
-        dots[slideIndex-1].className += " active";
-        setTimeout(showSlides, 3000); 
-        }
-        showSlides();*/
 
 
     
